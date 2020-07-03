b4195eea7139d121646ab1def623146e4f04c671
Aliaksandr Rykau <Aliaksandr_Rykau@epam.com>
2020-07-03 22:38:19 +0300

    Add GetLastIndexOfChar method with unit tests. Refactor code.

---
+++ RecursionIndexOfChar.Tests/GetIndexRecursivelyTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+#pragma warning disable CA1707
+
+namespace RecursionIndexOfChar.Tests
+{
+    [TestFixture]
+    public sealed class GetIndexRecursivelyTests
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        public void GetIndexOfChar_EmptyString_ThrowsException(string str)
+        {
+            Assert.Throws<ArgumentNullException>(() => GetIndexRecursively.GetIndexOfChar(str, 'a'));
+        }
+
+        [TestCase("abcdefghijklmnoprstquvwxyzabcdefghijklmnoprstquvwxyz", 'a', ExpectedResult = 0)]
+        [TestCase("abcdefghijklmnoprstquvwxyzabcdefghijklmnoprstquvwxyz", 'b', ExpectedResult = 1)]
+        [TestCase("abcdefghijklmnoprstquvwxyzabcdefghijklmnoprstquvwxyz", 'c', ExpectedResult = 2)]
+        [TestCase("abcdefghijklmnoprstquvwxyzabcdefghijklmnoprstquvwxyz", 'x', ExpectedResult = 23)]
+        [TestCase("abcdefghijklmnoprstquvwxyzabcdefghijklmnoprstquvwxyz", 'y', ExpectedResult = 24)]
+        [TestCase("abcdefghijklmnoprstquvwxyzabcdefghijklmnoprstquvwxyz", 'z', ExpectedResult = 25)]
+        [TestCase("abcefghijklmnoprstquvwxyzabcefghijklmnoprstquvwxyz", 'd', ExpectedResult = -1)]
+        public int GetIndexOfChar_NonEmptyString_ReturnsPosition(string str, char value)
+        {
+            return GetIndexRecursively.GetIndexOfChar(str, value);
+        }
+    }
+}
---
+++ RecursionIndexOfChar/GetIndexRecursively.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RecursionIndexOfChar
+{
+    public static class GetIndexRecursively
+    {
+        public static int GetIndexOfChar(string str, char value)
+        {
+            // TODO #1. Analyze the implementation of "GetLastIndexOfChar(string, char)" method, and implement the method using recursion.
+            throw new NotImplementedException();
+        }
+
+        public static int GetIndexOfChar(string str, char value, int startIndex, int count)
+        {
+            // TODO #2. Analyze the implementation of "GetLastIndexOfChar(string, char, int, int)" method, and implement the method using recursion.
+            throw new NotImplementedException();
+        }
+    }
+}
